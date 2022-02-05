dd95bfb416483fea07dc96b34fbf1b36f8067648
caiooliveirah09 <97924292+caiooliveirah09@users.noreply.github.com>
2022-02-05 11:55:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function retroceder() {
 function play() {
     const video2 = document.getElementById("audio1");
     const foto = document.getElementById("play").src;
-
+    console.log(foto)
+    const teste = window.location.origin + "/img/play.png"
+    console.log(teste)
     if (foto === window.location.origin + "/img/play.png") {
         video2.play();
         document.getElementById("play").src = "./img/pause.png";
