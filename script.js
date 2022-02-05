22351b3d3a78abbdc24886b80f105e26cf1758e9
caiooliveirah09 <leelros@hotmail.com>
2022-02-05 00:24:21 -0300

    fix(script.js): fixed src route

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-function retroceder(){
+function retroceder() {
     const video2 = document.getElementById("audio1");
     video2.currentTime = 0;
 }
-function play(){
+function play() {
     const video2 = document.getElementById("audio1");
     const foto = document.getElementById("play").src;
-    console.log(foto)
-    if(foto === "https://caiooliveirah09.github.io/musicPlayer/img/play.png"){
+
+    if (foto === window.location.origin + "/img/play.png") {
         video2.play();
-        document.getElementById("play").src="./img/pause.png";
-        console.log(foto)
-    }else{
+        document.getElementById("play").src = "./img/pause.png";
+    } else {
         video2.pause();
-        document.getElementById("play").src="./img/play.png";
+        document.getElementById("play").src = "./img/play.png";
     }
 
 }
