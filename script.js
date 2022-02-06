b22cf74183f8a8fd24cdb7d8a3e4db149a356da3
caiooliveirah09 <97924292+caiooliveirah09@users.noreply.github.com>
2022-02-05 22:29:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ function retroceder() {
 function play() {
     const video2 = document.getElementById("audio1");
     const foto = document.getElementById("play").src;
-    const host = window.location.host + "/img/play.png"
-    console.log(host)
     console.log(foto)
-
-    if (foto === "http://" + window.location.host + "/img/play.png") {
+    if (foto === "https://caiooliveirah09.github.io/musicPlayer/img/play.png") {
         video2.play();
         document.getElementById("play").src = "./img/pause.png";
     } else {
         video2.pause();
         document.getElementById("play").src = "./img/play.png";
     }
-
 }
